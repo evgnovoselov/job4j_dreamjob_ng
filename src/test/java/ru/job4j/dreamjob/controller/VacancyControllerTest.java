0cde6e8919c8debfdb72a64a90ba6f9527c14e14
Evgeny Novoselov <enovoselov@yahoo.com>
2023-05-07 23:19:32 +0300

    Добавил тестовые методы на контроллер VacancyController

---
+++ src/test/java/ru/job4j/dreamjob/controller/VacancyControllerTest.java
@@ -0,0 +1,131 @@
+package ru.job4j.dreamjob.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.web.multipart.MultipartFile;
+import ru.job4j.dreamjob.dto.FileDto;
+import ru.job4j.dreamjob.model.City;
+import ru.job4j.dreamjob.model.Vacancy;
+import ru.job4j.dreamjob.service.CityService;
+import ru.job4j.dreamjob.service.VacancyService;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class VacancyControllerTest {
+    private VacancyService vacancyService;
+    private CityService cityService;
+    private VacancyController vacancyController;
+    private MultipartFile testFile;
+
+    @BeforeEach
+    public void initServices() {
+        vacancyService = mock(VacancyService.class);
+        cityService = mock(CityService.class);
+        vacancyController = new VacancyController(vacancyService, cityService);
+        testFile = new MockMultipartFile("testFile.jpg", new byte[]{1, 2, 3});
+    }
+
+    @Test
+    public void whenRequestVacancyListPageThenGetPageWithVacancies() {
+        Vacancy vacancy1 = new Vacancy(1, "title 1", "description 1", 1, 2, true, LocalDateTime.now());
+        Vacancy vacancy2 = new Vacancy(2, "title 2", "description 2", 3, 4, true, LocalDateTime.now());
+        List<Vacancy> expectedVacancies = List.of(vacancy1, vacancy2);
+        when(vacancyService.findAll()).thenReturn(expectedVacancies);
+
+        ConcurrentModel model = new ConcurrentModel();
+        String view = vacancyController.getAll(model);
+        Collection<Vacancy> actualVacancies = (Collection<Vacancy>) model.getAttribute("vacancies");
+
+        assertThat(view).isEqualTo("vacancies/list");
+        assertThat(actualVacancies).isEqualTo(expectedVacancies);
+    }
+
+    @Test
+    public void whenRequestVacancyCreationPageThenGetPageWithCities() {
+        City city1 = new City(1, "Moscow");
+        City city2 = new City(2, "Санкт-Петербург");
+        List<City> expectedCities = List.of(city1, city2);
+        when(cityService.findAll()).thenReturn(expectedCities);
+
+        ConcurrentModel model = new ConcurrentModel();
+        String view = vacancyController.getCreationPage(model);
+        List<City> actualCities = (List<City>) model.getAttribute("cities");
+
+        assertThat(view).isEqualTo("vacancies/create");
+        assertThat(actualCities).isEqualTo(expectedCities);
+    }
+
+    @Test
+    public void whenPostVacancyWithFileThenSameDataAndRedirectToVacanciesPage() throws IOException {
+        Vacancy vacancy = new Vacancy(1, "title 1", "description 1", 1, 2, true, LocalDateTime.now());
+        FileDto fileDto = new FileDto(testFile.getOriginalFilename(), testFile.getBytes());
+        ArgumentCaptor<Vacancy> vacancyArgumentCaptor = ArgumentCaptor.forClass(Vacancy.class);
+        ArgumentCaptor<FileDto> fileDtoArgumentCaptor = ArgumentCaptor.forClass(FileDto.class);
+        when(vacancyService.save(vacancyArgumentCaptor.capture(), fileDtoArgumentCaptor.capture())).thenReturn(vacancy);
+
+        ConcurrentModel model = new ConcurrentModel();
+        String view = vacancyController.create(vacancy, testFile, model);
+        Vacancy actualVacancy = vacancyArgumentCaptor.getValue();
+        FileDto actualFileDto = fileDtoArgumentCaptor.getValue();
+
+        assertThat(view).isEqualTo("redirect:/vacancies");
+        assertThat(actualVacancy).isEqualTo(vacancy);
+        assertThat(actualFileDto).usingRecursiveComparison().isEqualTo(fileDto);
+    }
+
+    @Test
+    public void whenSomeExceptionThrownThenGetErrorPageWithMessage() {
+        RuntimeException expectedException = new RuntimeException("Failed to write file");
+        when(vacancyService.save(any(), any())).thenThrow(expectedException);
+
+        ConcurrentModel model = new ConcurrentModel();
+        String view = vacancyController.create(new Vacancy(), testFile, model);
+        String actualExceptionMessage = (String) model.getAttribute("message");
+
+        assertThat(view).isEqualTo("errors/404");
+        assertThat(actualExceptionMessage).isEqualTo(expectedException.getMessage());
+    }
+
+    @Test
+    public void whenGetVacancyByIdThenGetVacancyAndCities() {
+        City city1 = new City(1, "Moscow");
+        City city2 = new City(2, "Санкт-Петербург");
+        List<City> cities = List.of(city1, city2);
+        when(cityService.findAll()).thenReturn(cities);
+        Vacancy vacancy = new Vacancy(1, "title 1", "description 1", 1, 2, true, LocalDateTime.now());
+        when(vacancyService.findById(any(Integer.class))).thenReturn(Optional.of(vacancy));
+
+        ConcurrentModel model = new ConcurrentModel();
+        String view = vacancyController.getById(model, 1);
+        Vacancy actualVacancy = (Vacancy) model.getAttribute("vacancy");
+        List<City> actualCities = (List<City>) model.getAttribute("cities");
+
+        assertThat(view).isEqualTo("vacancies/one");
+        assertThat(actualVacancy).isEqualTo(vacancy);
+        assertThat(actualCities).isEqualTo(cities);
+    }
+
+    @Test
+    public void whenGetVacancyByIdButThereNotHaveThenGetErrorPageWithMessage() {
+        when(vacancyService.findById(any(Integer.class))).thenReturn(Optional.empty());
+
+        ConcurrentModel model = new ConcurrentModel();
+        String view = vacancyController.getById(model, 1);
+        String message = (String) model.getAttribute("message");
+
+        assertThat(view).isEqualTo("errors/404");
+        assertThat(message).isEqualTo("Вакансия с указанным идентификатором не найдена");
+    }
+}
